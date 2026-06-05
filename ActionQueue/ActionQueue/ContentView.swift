7e80beb443edd53e1c809051ae81f28eaebae16b
Arsenty Halimovsky <ghost12@yandex.ru>
2026-06-05 06:46:26 +0300

    add Action

--- ActionQueue/ActionQueue/ContentView.swift
+++ ActionQueue/ActionQueue/ContentView.swift
@@ -1,17 +1,29 @@
 /* TODO:
- - enum "EnemyAction" ❌
+ - enum "Action" ✅
  - Method to generate EnemyAction ❌
  - Queue data structure to hold actions ❌
  - Method to execute actions from the queue ❌
  - AsyncAwait for simulation of delays ❌
  - Visual representation of different actions on the view and with it's execution
+ 
+ Try to simulate an antire cycle of enemy action queue with sumulated effects and delay
+ also try flask wasActionStarted and wasActionEnded
  */
 
 import SwiftUI
 
+// A simple model to represent an actual action
+
+struct Action {
+	
+	var name: String
+	var actionType: ActionType
+	var timeToCast: Double
+}
+
 // Any type of character actions in the battle
 
-enum Action: String {
+enum ActionType: String {
 	
 	case fastAttack
 	case slowAttack
@@ -55,26 +67,28 @@ struct ContentView: View {
 			
 			Button("Add Action") {
 				
-				var action: Action
+				var actionType: ActionType
 				
 				let roll = Int.random(in: 1...5)
 				
 				switch roll {
-				case 1: action = Action.fastAttack
-				case 2: action = Action.slowAttack
-				case 3: action = Action.heal
-				case 4: action = Action.block
-				case 5: action = Action.ultimate
+				case 1: actionType = ActionType.fastAttack
+				case 2: actionType = ActionType.slowAttack
+				case 3: actionType = ActionType.heal
+				case 4: actionType = ActionType.block
+				case 5: actionType = ActionType.ultimate
 				default:
-					action = Action.fastAttack
+					actionType = ActionType.fastAttack
 				}
+				
+				let action = Action(name: actionType.rawValue, actionType: actionType, timeToCast: 2.0)
 				
 				actionQueue.addAction(action: action)
 			}
 			
 			Button("Execute Action") {
 				guard let action = actionQueue.executeAction() else { return }
-				print(action.rawValue)
+				print(action.name)
 			}
 		}
     }
