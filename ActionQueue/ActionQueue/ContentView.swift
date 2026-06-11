7a54f048aa5facb0e147c95d6edd8a7f19aa5a8d
Arsenty Halimovsky <ghost12@yandex.ru>
2026-06-11 10:15:03 +0300

    Update ActionQueue

--- ActionQueue/ActionQueue/ContentView.swift
+++ ActionQueue/ActionQueue/ContentView.swift
@@ -1,28 +1,40 @@
-/* TODO:
- - enum "Action" ✅
- - Method to generate EnemyAction ❌
- - Queue data structure to hold actions ❌
- - Method to execute actions from the queue ❌
- - AsyncAwait for simulation of delays ❌
- - Visual representation of different actions on the view and with it's execution
+/* Algorithm of execution:
  
- Try to simulate an antire cycle of enemy action queue with sumulated effects and delay
- also try flask wasActionStarted and wasActionEnded
+ 1. Pass turn to enemy
+ 2. Generate enemy actions (accordingly to energy capacity)
+ 3. Add actions to the queue
+ 4. Check if queue is not empty
+ 5. Grab first action from the queue
+ 6. Set flag actionInProgress to true
+ 7. Execute action
+ 8. Set flag actionInProgress to false
+ 9. Go back to step 4 until it empty
  */
 
 import SwiftUI
 
-// A simple model to represent an actual action
+// MARK: Action struct
 
+/// A simple model to represent an actual action
 struct Action {
 	
 	var name: String
 	var actionType: ActionType
 	var timeToCast: Double
+	
+	init(name: String,
+		 actionType: ActionType,
+		 timeToCast: Double
+	) {
+		self.name = name
+		self.actionType = actionType
+		self.timeToCast = timeToCast
+	}
 }
 
-// Any type of character actions in the battle
+// MARK: ActionType enum
 
+/// Any type of character actions in the battle
 enum ActionType: String {
 	
 	case fastAttack
@@ -32,64 +44,113 @@ enum ActionType: String {
 	case ultimate
 }
 
-// We need a queue to fill with actions
+// MARK: ActionQueue struct
 
-struct ActionQueue<Action> {
+/// We need a queue to fill with actions
+class ActionQueue {
 	
 	var queue: [Action] = []
+	
+	var isEmpty: Bool {
+		queue.isEmpty
+	}
+	
+	var actionInProgress = false
 	
 	var count: Int {
 		queue.count
 	}
 	
-	mutating func addAction(action: Action) {
+	/// Method runs until queue will be empty
+	/// You do not need guard in execute/extract action because if it's empty it won't move further
+	func executeActions() {
+		
+		print("Start Running the queue")
+		
+		while queue.isEmpty != true {
+			
+			print("Found an action")
+			
+			if actionInProgress != true {
+				
+				let action = extractAction()
+				print("Passed action for execution")
+				executeSingleAction(action)
+			}
+		}
+	}
+	
+	func addAction(action: Action) {
 		queue.append(action)
 	}
 	
-	mutating func executeAction() -> Action? {
+	func extractAction() -> Action {
+		queue.removeFirst()
+	}
+	
+	func executeSingleAction(_ action: Action) {
 		
-		guard !queue.isEmpty else {
-			// pass turn to hero
-			return nil
+		print("Execution started")
+		actionInProgress = true
+		print("execution continue")
+		
+		DispatchQueue.main.asyncAfter(deadline: .now() + action.timeToCast) {
+			self.actionInProgress = false
+			print("action has been executed after \(action.timeToCast) seconds")
 		}
-		return queue.removeFirst()
 	}
 }
 
+// MARK: Content View
+
 struct ContentView: View {
 	
-	@State var actionQueue = ActionQueue<Action>()
+	@State var actionQueue = ActionQueue()
 	
-    var body: some View {
+	var body: some View {
 		
 		VStack {
 			Text("Action Queue. (\(actionQueue.count))")
 			
 			Button("Add Action") {
+				print("ARE YOU HERE YOU FUCKING MORON?")
 				
-				var actionType: ActionType
+				var actionType = ActionType.fastAttack
+				var timeToCast = 0.0
 				
 				let roll = Int.random(in: 1...5)
 				
 				switch roll {
-				case 1: actionType = ActionType.fastAttack
-				case 2: actionType = ActionType.slowAttack
-				case 3: actionType = ActionType.heal
-				case 4: actionType = ActionType.block
-				case 5: actionType = ActionType.ultimate
+					
+				case 1:
+					actionType = ActionType.fastAttack
+					timeToCast = 1.0
+				case 2:
+					actionType = ActionType.slowAttack
+					timeToCast = 2.0
+				case 3:
+					actionType = ActionType.heal
+					timeToCast = 1.0
+				case 4:
+					actionType = ActionType.block
+					timeToCast = 1.0
+				case 5:
+					actionType = ActionType.ultimate
+					timeToCast = 3.0
 				default:
 					actionType = ActionType.fastAttack
+					timeToCast = 1.0
 				}
 				
-				let action = Action(name: actionType.rawValue, actionType: actionType, timeToCast: 2.0)
+				let action = Action(name: actionType.rawValue, actionType: actionType, timeToCast: timeToCast)
 				
 				actionQueue.addAction(action: action)
+				print("\(action) has been added")
 			}
 			
-			Button("Execute Action") {
-				guard let action = actionQueue.executeAction() else { return }
-				print(action.name)
+			Button("Execute Actions") {
+				actionQueue.executeActions()
 			}
 		}
-    }
+	}
 }
